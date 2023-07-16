b8f1e37334121930c5eca8ac1c2e9e8dcb7aa810
LJ Ensomo <ensomolj@gmail.com>
2023-07-16 09:23:39 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - Project Management</title>
+</head>
+<body>
+    <h2>Project Management</h2><hr>
+    <p>This is created with PHP, HTML5, CSS, JAVASCRIPT. Build started on July 16, 2023</p>
+</body>
+</html>
