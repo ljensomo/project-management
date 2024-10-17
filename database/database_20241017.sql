d4465d527363b304732fe0db854dc1180614fde2
LJ Ensomo <ensomolj@gmail.com>
2024-10-17 22:29:29 +0800

    development: database structure backup

---
+++ database/database_20241017.sql
@@ -0,0 +1,131 @@
+/*
+SQLyog Professional v13.1.1 (64 bit)
+MySQL - 10.4.22-MariaDB : Database - db_project_management
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_project_management` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `db_project_management`;
+
+/*Table structure for table `module_functions` */
+
+DROP TABLE IF EXISTS `module_functions`;
+
+CREATE TABLE `module_functions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `module_id` int(11) NOT NULL,
+  `function_name` varchar(150) NOT NULL,
+  `function_description` text DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `date_created` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `project_modules` */
+
+DROP TABLE IF EXISTS `project_modules`;
+
+CREATE TABLE `project_modules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` tinyint(4) NOT NULL,
+  `module_name` varchar(150) NOT NULL,
+  `module_description` text DEFAULT NULL,
+  `date_created` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `project_owners` */
+
+DROP TABLE IF EXISTS `project_owners`;
+
+CREATE TABLE `project_owners` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` tinyint(3) NOT NULL,
+  `owner_id` tinyint(3) NOT NULL,
+  `date_created` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `projects` */
+
+DROP TABLE IF EXISTS `projects`;
+
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(150) NOT NULL,
+  `project_description` text NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
+  `date_created` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `ticket_categories` */
+
+DROP TABLE IF EXISTS `ticket_categories`;
+
+CREATE TABLE `ticket_categories` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `ticket_statuses` */
+
+DROP TABLE IF EXISTS `ticket_statuses`;
+
+CREATE TABLE `ticket_statuses` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `tickets` */
+
+DROP TABLE IF EXISTS `tickets`;
+
+CREATE TABLE `tickets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` tinyint(4) NOT NULL,
+  `module_id` int(11) DEFAULT NULL,
+  `category_id` tinyint(4) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `assigned_to` tinyint(4) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
+  `created_by` tinyint(4) NOT NULL,
+  `date_created` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `role_id` tinyint(1) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` text NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `date_created` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
