c57bc3dc99db3714555a51fe01c58b8612a119cf
duasosandy <sandyduaso09@gmail.com>
2025-03-22 09:14:25 +0800

    build: modify

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./resources/views/**/*.php", // For PHP views in the views directory
     "./resources/views/**/*.html", // If you use HTML files as well
+    "./resources/views/*.php", // If you use HTML files as well
     "**/*.php",
   ],
   darkMode: "class",
