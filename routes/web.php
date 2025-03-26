03a4118bc1e8db629b974c7d31ec40c204c467bb
duasosandy <sandyduaso09@gmail.com>
2025-03-26 21:07:00 +0800

    add some routes

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Core\Route;
 
 Route::get('/', 'HomeController@index');
+// Auth
 Route::get('/login', 'AuthController@index');
+Route::post('/login', 'AuthController@login');
+// Project
+Route::get('/projects', 'ProjectController@index');
