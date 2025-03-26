c036513d8e6ff474817c5812d411f8b2f604b9ca
duasosandy <sandyduaso09@gmail.com>
2025-03-26 20:40:06 +0800

    add config setup

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'connections' => [
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'port'      => env('DB_PORT', '3306'),
+            'database'  => env('DB_NAME', 'phpmvc'),
+            'username'  => env('DB_USER', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix'    => '',
+            'strict'    => true,
+            'engine'    => null,
+        ]
+    ]
+];
--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,33 @@
 
 use Core\View;
 
+if (!function_exists('config')) {
+    function config(string $file, string $key, $default = null)
+    {
+        $path = APP_ROOT . "config/{$file}.php";
+
+        if (!file_exists($path)) {
+            throw new \Exception("Configuration file '{$file}' not found.");
+        }
+
+        $data = require $path;
+
+        $keys = explode('.', $key);
+        foreach ($keys as $keyPart) {
+            if (isset($data[$keyPart])) {
+                $data = $data[$keyPart];
+            } else {
+                if ($default !== null) {
+                    return $default;
+                }
+                throw new \Exception("Configuration key '{$key}' not found in '{$file}'");
+            }
+        }
+
+        return $data;
+    }
+}
+
 if (!function_exists('env')) {
     function env($key, $default = null)
     {
