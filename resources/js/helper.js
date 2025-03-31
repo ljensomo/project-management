a14eefa050f76a886b072dbf650851dc37553212
duasosandy <sandyduaso09@gmail.com>
2025-03-31 08:58:35 +0800

    update datatable and add generate buttons

--- resources/js/helper.js
+++ resources/js/helper.js
@@ -70,7 +70,12 @@ export function instantiateDatatable(parameters) {
       const data = response.data.data;
 
       const tableData = data.map((row) => {
-        return parameter.columns.map((col) => row[col.data]);
+        return parameter.columns.map((col) => {
+          if (typeof col.data === "function") {
+            return col.data(row);
+          }
+          return row[col.data];
+        });
       });
 
       const table = document.querySelector(parameter.tableId);
@@ -113,4 +118,41 @@ export function observeTableRendering(tableId, callback) {
   });
 
   observer.observe(tableElement, { childList: true, subtree: true });
+}
+
+export function generateTableRowButtons(parameters) {
+  let buttons = "";
+
+  if (parameters.view) {
+    buttons += `
+      <a href="#" class="inline-flex items-center justify-center rounded-lg p-2 bg-cyan-500 hover:bg-cyan-600 focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-700 dark:hover:bg-cyan-800 dark:focus:ring-cyan-800">
+        <svg class="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+          <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z" clip-rule="evenodd"/>
+        </svg>
+      </a>
+    `;
+  }
+
+  if (parameters.edit) {
+    buttons += `
+      <a href="#" class="inline-flex items-center justify-center bg-yellow-400 hover:bg-yellow-500 dark:focus:ring-yellow-900 focus:ring-4 rounded-lg p-2 focus:outline-none">
+        <svg class="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+          <path fill-rule="evenodd" d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z" clip-rule="evenodd"/>
+          <path fill-rule="evenodd" d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z" clip-rule="evenodd"/>
+        </svg>
+      </a>
+    `;
+  }
+
+  if (parameters.delete) {
+    buttons += `
+      <a href="#" class="inline-flex items-center justify-center bg-red-700 hover:bg-red-800 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 focus:ring-4 rounded-lg p-2 focus:outline-none">
+        <svg class="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+          <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
+        </svg>
+      </a>
+    `;
+  }
+
+  return buttons;
 }
